1a2a0a1e768bf9f4f60550e0c8caa4f4ddb46cdb
Даниил <reflash-ne@yandex.ru>
2016-06-09 18:12:31 +0300

    ST-2 Design database basic structures. Made the models, need to fix some display for those models

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from api.models import Schedule
 
 
-def index(request, group_id):
+def detail(request, group_id):
     groups = Schedule.objects.order_by('-group')
     context = {'groups': groups}
     return render(request, 'schedule/index.html', context)
