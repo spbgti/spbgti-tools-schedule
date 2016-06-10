045d4a5988c6ba6e215e13cbb1d5c6a8101e9b2c
Даниил <reflash-ne@yandex.ru>
2016-06-10 15:53:40 +0300

    ST-5 Fix several issues with models display from admin

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from unittest import TestCase
 
-# Create your tests here.
+from core.models import Room, Location
+
+
+class RoomTestCase(TestCase):
+    location_name = "Кафедра САПРиУ"
+    room_name = "ауд. 6"
+
+    def setUp(self):
+        Room.objects.create(name=self.room_name, location=Location.objects.create
+        (name=self.location_name, geo_position="None"))
+
+    def test_room_str_is_equal_to_location_str(self):
+        room = Room.objects.get(pk=1)
+        self.assertEqual(str(room), self.location_name)
