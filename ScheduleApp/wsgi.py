9cce7151cfa5a09c10d4ad064f84e8a09319ce35
Даниил <reflash-ne@yandex.ru>
2016-06-11 15:20:21 +0300

    CI. Set files for Heroku

--- ScheduleApp/wsgi.py
+++ ScheduleApp/wsgi.py
@@ -12,5 +12,14 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScheduleApp.settings")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 application = get_wsgi_application()
