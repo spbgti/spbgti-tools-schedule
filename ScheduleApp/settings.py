1068779c249613967cc1c0caf75bd6074b1b2c27
Даниил <reflash-ne@yandex.ru>
2016-06-12 20:32:56 +0300

    CI. Set files for Heroku

--- ScheduleApp/settings.py
+++ ScheduleApp/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 
 # Update database configuration with $DATABASE_URL.
