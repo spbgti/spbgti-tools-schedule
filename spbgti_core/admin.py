bae9e4d55f7b8b09e1ad680b60b02e27fcb7eac6
Даниил <reflash-ne@yandex.ru>
2016-06-15 01:10:12 +0300

    Revert "Correct directories"
    This reverts commit b3205439dc3b0d0e6c3c134a99e59de1eaf4292a.
    
    REVERT CHANGES

---
+++ spbgti_core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Group
+from .models import Location
+from .models import Room
+from .models import Semester
+from .models import Teacher
+
+admin.site.register(Group)
+admin.site.register(Location)
+admin.site.register(Room)
+admin.site.register(Teacher)
+admin.site.register(Semester)
