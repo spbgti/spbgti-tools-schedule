2081744a96ab3ff04e2299b84b2468bf71722728
Даниил <reflash-ne@yandex.ru>
2016-06-15 16:55:10 +0300

    Correct shell script

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 python remove_migrations.py
-python manage.py makemigrations
+python manage.py makemigrations api
+python manage.py makemigrations core
 python manage.py migrate
