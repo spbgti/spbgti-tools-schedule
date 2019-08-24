4f2ad9091d98e98e7c11981458114561c5371c30
Dima Boger <kotvberloge@gmail.com>
2019-08-24 08:49:40 -0700

    Travis: update python version, remove old deploy and check scripts (#38)

--- heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Change this when it will come for real production
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
-python manage.py migrate api
-python manage.py migrate core
