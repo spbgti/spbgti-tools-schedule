1692678142d10f01e19dd210795d8e92084533e0
Boger <kotvberloge@gmail.com>
2019-08-31 16:23:13 +0300

    :white_check_mark: Add snapshot tests for groups

---
+++ tests/snapshots/snap_test_groups.py
@@ -0,0 +1,867 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_one_group 1'] = [
+    {
+        'group_id': 1,
+        'number': '446'
+    }
+]
+
+snapshots['test_few_groups 1'] = [
+    {
+        'group_id': 1,
+        'number': '443'
+    },
+    {
+        'group_id': 2,
+        'number': '446'
+    },
+    {
+        'group_id': 3,
+        'number': '1020'
+    }
+]
+
+snapshots['test_many_groups 1'] = [
+    {
+        'group_id': 1,
+        'number': '0'
+    },
+    {
+        'group_id': 2,
+        'number': '1'
+    },
+    {
+        'group_id': 3,
+        'number': '2'
+    },
+    {
+        'group_id': 4,
+        'number': '3'
+    },
+    {
+        'group_id': 5,
+        'number': '4'
+    },
+    {
+        'group_id': 6,
+        'number': '5'
+    },
+    {
+        'group_id': 7,
+        'number': '6'
+    },
+    {
+        'group_id': 8,
+        'number': '7'
+    },
+    {
+        'group_id': 9,
+        'number': '8'
+    },
+    {
+        'group_id': 10,
+        'number': '9'
+    },
+    {
+        'group_id': 11,
+        'number': '10'
+    },
+    {
+        'group_id': 12,
+        'number': '11'
+    },
+    {
+        'group_id': 13,
+        'number': '12'
+    },
+    {
+        'group_id': 14,
+        'number': '13'
+    },
+    {
+        'group_id': 15,
+        'number': '14'
+    },
+    {
+        'group_id': 16,
+        'number': '15'
+    },
+    {
+        'group_id': 17,
+        'number': '16'
+    },
+    {
+        'group_id': 18,
+        'number': '17'
+    },
+    {
+        'group_id': 19,
+        'number': '18'
+    },
+    {
+        'group_id': 20,
+        'number': '19'
+    },
+    {
+        'group_id': 21,
+        'number': '20'
+    },
+    {
+        'group_id': 22,
+        'number': '21'
+    },
+    {
+        'group_id': 23,
+        'number': '22'
+    },
+    {
+        'group_id': 24,
+        'number': '23'
+    },
+    {
+        'group_id': 25,
+        'number': '24'
+    },
+    {
+        'group_id': 26,
+        'number': '25'
+    },
+    {
+        'group_id': 27,
+        'number': '26'
+    },
+    {
+        'group_id': 28,
+        'number': '27'
+    },
+    {
+        'group_id': 29,
+        'number': '28'
+    },
+    {
+        'group_id': 30,
+        'number': '29'
+    },
+    {
+        'group_id': 31,
+        'number': '30'
+    },
+    {
+        'group_id': 32,
+        'number': '31'
+    },
+    {
+        'group_id': 33,
+        'number': '32'
+    },
+    {
+        'group_id': 34,
+        'number': '33'
+    },
+    {
+        'group_id': 35,
+        'number': '34'
+    },
+    {
+        'group_id': 36,
+        'number': '35'
+    },
+    {
+        'group_id': 37,
+        'number': '36'
+    },
+    {
+        'group_id': 38,
+        'number': '37'
+    },
+    {
+        'group_id': 39,
+        'number': '38'
+    },
+    {
+        'group_id': 40,
+        'number': '39'
+    },
+    {
+        'group_id': 41,
+        'number': '40'
+    },
+    {
+        'group_id': 42,
+        'number': '41'
+    },
+    {
+        'group_id': 43,
+        'number': '42'
+    },
+    {
+        'group_id': 44,
+        'number': '43'
+    },
+    {
+        'group_id': 45,
+        'number': '44'
+    },
+    {
+        'group_id': 46,
+        'number': '45'
+    },
+    {
+        'group_id': 47,
+        'number': '46'
+    },
+    {
+        'group_id': 48,
+        'number': '47'
+    },
+    {
+        'group_id': 49,
+        'number': '48'
+    },
+    {
+        'group_id': 50,
+        'number': '49'
+    },
+    {
+        'group_id': 51,
+        'number': '50'
+    },
+    {
+        'group_id': 52,
+        'number': '51'
+    },
+    {
+        'group_id': 53,
+        'number': '52'
+    },
+    {
+        'group_id': 54,
+        'number': '53'
+    },
+    {
+        'group_id': 55,
+        'number': '54'
+    },
+    {
+        'group_id': 56,
+        'number': '55'
+    },
+    {
+        'group_id': 57,
+        'number': '56'
+    },
+    {
+        'group_id': 58,
+        'number': '57'
+    },
+    {
+        'group_id': 59,
+        'number': '58'
+    },
+    {
+        'group_id': 60,
+        'number': '59'
+    },
+    {
+        'group_id': 61,
+        'number': '60'
+    },
+    {
+        'group_id': 62,
+        'number': '61'
+    },
+    {
+        'group_id': 63,
+        'number': '62'
+    },
+    {
+        'group_id': 64,
+        'number': '63'
+    },
+    {
+        'group_id': 65,
+        'number': '64'
+    },
+    {
+        'group_id': 66,
+        'number': '65'
+    },
+    {
+        'group_id': 67,
+        'number': '66'
+    },
+    {
+        'group_id': 68,
+        'number': '67'
+    },
+    {
+        'group_id': 69,
+        'number': '68'
+    },
+    {
+        'group_id': 70,
+        'number': '69'
+    },
+    {
+        'group_id': 71,
+        'number': '70'
+    },
+    {
+        'group_id': 72,
+        'number': '71'
+    },
+    {
+        'group_id': 73,
+        'number': '72'
+    },
+    {
+        'group_id': 74,
+        'number': '73'
+    },
+    {
+        'group_id': 75,
+        'number': '74'
+    },
+    {
+        'group_id': 76,
+        'number': '75'
+    },
+    {
+        'group_id': 77,
+        'number': '76'
+    },
+    {
+        'group_id': 78,
+        'number': '77'
+    },
+    {
+        'group_id': 79,
+        'number': '78'
+    },
+    {
+        'group_id': 80,
+        'number': '79'
+    },
+    {
+        'group_id': 81,
+        'number': '80'
+    },
+    {
+        'group_id': 82,
+        'number': '81'
+    },
+    {
+        'group_id': 83,
+        'number': '82'
+    },
+    {
+        'group_id': 84,
+        'number': '83'
+    },
+    {
+        'group_id': 85,
+        'number': '84'
+    },
+    {
+        'group_id': 86,
+        'number': '85'
+    },
+    {
+        'group_id': 87,
+        'number': '86'
+    },
+    {
+        'group_id': 88,
+        'number': '87'
+    },
+    {
+        'group_id': 89,
+        'number': '88'
+    },
+    {
+        'group_id': 90,
+        'number': '89'
+    },
+    {
+        'group_id': 91,
+        'number': '90'
+    },
+    {
+        'group_id': 92,
+        'number': '91'
+    },
+    {
+        'group_id': 93,
+        'number': '92'
+    },
+    {
+        'group_id': 94,
+        'number': '93'
+    },
+    {
+        'group_id': 95,
+        'number': '94'
+    },
+    {
+        'group_id': 96,
+        'number': '95'
+    },
+    {
+        'group_id': 97,
+        'number': '96'
+    },
+    {
+        'group_id': 98,
+        'number': '97'
+    },
+    {
+        'group_id': 99,
+        'number': '98'
+    },
+    {
+        'group_id': 100,
+        'number': '99'
+    },
+    {
+        'group_id': 101,
+        'number': '100'
+    },
+    {
+        'group_id': 102,
+        'number': '101'
+    },
+    {
+        'group_id': 103,
+        'number': '102'
+    },
+    {
+        'group_id': 104,
+        'number': '103'
+    },
+    {
+        'group_id': 105,
+        'number': '104'
+    },
+    {
+        'group_id': 106,
+        'number': '105'
+    },
+    {
+        'group_id': 107,
+        'number': '106'
+    },
+    {
+        'group_id': 108,
+        'number': '107'
+    },
+    {
+        'group_id': 109,
+        'number': '108'
+    },
+    {
+        'group_id': 110,
+        'number': '109'
+    },
+    {
+        'group_id': 111,
+        'number': '110'
+    },
+    {
+        'group_id': 112,
+        'number': '111'
+    },
+    {
+        'group_id': 113,
+        'number': '112'
+    },
+    {
+        'group_id': 114,
+        'number': '113'
+    },
+    {
+        'group_id': 115,
+        'number': '114'
+    },
+    {
+        'group_id': 116,
+        'number': '115'
+    },
+    {
+        'group_id': 117,
+        'number': '116'
+    },
+    {
+        'group_id': 118,
+        'number': '117'
+    },
+    {
+        'group_id': 119,
+        'number': '118'
+    },
+    {
+        'group_id': 120,
+        'number': '119'
+    },
+    {
+        'group_id': 121,
+        'number': '120'
+    },
+    {
+        'group_id': 122,
+        'number': '121'
+    },
+    {
+        'group_id': 123,
+        'number': '122'
+    },
+    {
+        'group_id': 124,
+        'number': '123'
+    },
+    {
+        'group_id': 125,
+        'number': '124'
+    },
+    {
+        'group_id': 126,
+        'number': '125'
+    },
+    {
+        'group_id': 127,
+        'number': '126'
+    },
+    {
+        'group_id': 128,
+        'number': '127'
+    },
+    {
+        'group_id': 129,
+        'number': '128'
+    },
+    {
+        'group_id': 130,
+        'number': '129'
+    },
+    {
+        'group_id': 131,
+        'number': '130'
+    },
+    {
+        'group_id': 132,
+        'number': '131'
+    },
+    {
+        'group_id': 133,
+        'number': '132'
+    },
+    {
+        'group_id': 134,
+        'number': '133'
+    },
+    {
+        'group_id': 135,
+        'number': '134'
+    },
+    {
+        'group_id': 136,
+        'number': '135'
+    },
+    {
+        'group_id': 137,
+        'number': '136'
+    },
+    {
+        'group_id': 138,
+        'number': '137'
+    },
+    {
+        'group_id': 139,
+        'number': '138'
+    },
+    {
+        'group_id': 140,
+        'number': '139'
+    },
+    {
+        'group_id': 141,
+        'number': '140'
+    },
+    {
+        'group_id': 142,
+        'number': '141'
+    },
+    {
+        'group_id': 143,
+        'number': '142'
+    },
+    {
+        'group_id': 144,
+        'number': '143'
+    },
+    {
+        'group_id': 145,
+        'number': '144'
+    },
+    {
+        'group_id': 146,
+        'number': '145'
+    },
+    {
+        'group_id': 147,
+        'number': '146'
+    },
+    {
+        'group_id': 148,
+        'number': '147'
+    },
+    {
+        'group_id': 149,
+        'number': '148'
+    },
+    {
+        'group_id': 150,
+        'number': '149'
+    },
+    {
+        'group_id': 151,
+        'number': '150'
+    },
+    {
+        'group_id': 152,
+        'number': '151'
+    },
+    {
+        'group_id': 153,
+        'number': '152'
+    },
+    {
+        'group_id': 154,
+        'number': '153'
+    },
+    {
+        'group_id': 155,
+        'number': '154'
+    },
+    {
+        'group_id': 156,
+        'number': '155'
+    },
+    {
+        'group_id': 157,
+        'number': '156'
+    },
+    {
+        'group_id': 158,
+        'number': '157'
+    },
+    {
+        'group_id': 159,
+        'number': '158'
+    },
+    {
+        'group_id': 160,
+        'number': '159'
+    },
+    {
+        'group_id': 161,
+        'number': '160'
+    },
+    {
+        'group_id': 162,
+        'number': '161'
+    },
+    {
+        'group_id': 163,
+        'number': '162'
+    },
+    {
+        'group_id': 164,
+        'number': '163'
+    },
+    {
+        'group_id': 165,
+        'number': '164'
+    },
+    {
+        'group_id': 166,
+        'number': '165'
+    },
+    {
+        'group_id': 167,
+        'number': '166'
+    },
+    {
+        'group_id': 168,
+        'number': '167'
+    },
+    {
+        'group_id': 169,
+        'number': '168'
+    },
+    {
+        'group_id': 170,
+        'number': '169'
+    },
+    {
+        'group_id': 171,
+        'number': '170'
+    },
+    {
+        'group_id': 172,
+        'number': '171'
+    },
+    {
+        'group_id': 173,
+        'number': '172'
+    },
+    {
+        'group_id': 174,
+        'number': '173'
+    },
+    {
+        'group_id': 175,
+        'number': '174'
+    },
+    {
+        'group_id': 176,
+        'number': '175'
+    },
+    {
+        'group_id': 177,
+        'number': '176'
+    },
+    {
+        'group_id': 178,
+        'number': '177'
+    },
+    {
+        'group_id': 179,
+        'number': '178'
+    },
+    {
+        'group_id': 180,
+        'number': '179'
+    },
+    {
+        'group_id': 181,
+        'number': '180'
+    },
+    {
+        'group_id': 182,
+        'number': '181'
+    },
+    {
+        'group_id': 183,
+        'number': '182'
+    },
+    {
+        'group_id': 184,
+        'number': '183'
+    },
+    {
+        'group_id': 185,
+        'number': '184'
+    },
+    {
+        'group_id': 186,
+        'number': '185'
+    },
+    {
+        'group_id': 187,
+        'number': '186'
+    },
+    {
+        'group_id': 188,
+        'number': '187'
+    },
+    {
+        'group_id': 189,
+        'number': '188'
+    },
+    {
+        'group_id': 190,
+        'number': '189'
+    },
+    {
+        'group_id': 191,
+        'number': '190'
+    },
+    {
+        'group_id': 192,
+        'number': '191'
+    },
+    {
+        'group_id': 193,
+        'number': '192'
+    },
+    {
+        'group_id': 194,
+        'number': '193'
+    },
+    {
+        'group_id': 195,
+        'number': '194'
+    },
+    {
+        'group_id': 196,
+        'number': '195'
+    },
+    {
+        'group_id': 197,
+        'number': '196'
+    },
+    {
+        'group_id': 198,
+        'number': '197'
+    },
+    {
+        'group_id': 199,
+        'number': '198'
+    },
+    {
+        'group_id': 200,
+        'number': '199'
+    }
+]
+
+snapshots['test_not_only_numeric_group 1'] = [
+    {
+        'group_id': 1,
+        'number': '446м'
+    }
+]
+
+snapshots['test_basic_filtering 1'] = [
+    {
+        'group_id': 1,
+        'number': '444'
+    }
+]
+
+snapshots['test_no_one_groups 1'] = [
+]
+
+snapshots['test_case_sensetive_filtering 1'] = [
+    {
+        'group_id': 1,
+        'number': '444м'
+    }
+]
+
+snapshots['test_case_sensitive_filtering 1'] = [
+    {
+        'group_id': 1,
+        'number': '444м'
+    }
+]
+
+snapshots['test_empty_filtering 1'] = [
+]
